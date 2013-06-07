649a9d8025077f6e50d2b63c6629c5c34cc5d4a9
eli.t.desouza@gmail.com <eli.t.desouza@gmail.com@eaa37e75-6923-8a03-05c2-e88adbbf307e>
2013-06-07 00:38:14 +0000

    Vai e fuder gaspar.

--- trunk/Provisionamento/src/provisionamento/Provisionamento.java
+++ trunk/Provisionamento/src/provisionamento/Provisionamento.java
@@ -3,11 +3,5 @@ package provisionamento;
 public class Provisionamento {
 
     public static void main(String[] args) {
-       //Eli é um boioloa
-    }
-    
-    public boolean eliEhUmViado()
-    {
-        return true;
     }
 }
