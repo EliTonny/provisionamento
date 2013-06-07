1ac864a5ab9adf215ce9b670eb24f2772e150182
gustavo.sabel.gs <gustavo.sabel.gs@gmail.com>
2013-06-07 00:31:38 +0000

    Apagay os comenários lol

--- Provisionamento/src/provisionamento/Provisionamento.java
+++ Provisionamento/src/provisionamento/Provisionamento.java
@@ -1,19 +1,8 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package provisionamento;
 
-/**
- *
- * @author Game Mania
- */
 public class Provisionamento {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        /*fadsfasfasd*/
+     
     }
 }
