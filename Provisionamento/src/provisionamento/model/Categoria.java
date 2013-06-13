dd1493b4ded4e377fd65d6f5fb70aa430be52ad5
eli.t.desouza <eli.t.desouza@gmail.com>
2013-06-13 23:49:16 +0000

    Carga inicial.

---
+++ Provisionamento/src/provisionamento/model/Categoria.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package provisionamento.model;
+
+/**
+ *
+ * @author Eli T. de Souza
+ */
+public class Categoria {
+    private String descricao;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
